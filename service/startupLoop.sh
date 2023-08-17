e66177c48a04c525d89bffd081ac01b091151045
Zack <jzwoodruff@gmail.com>
2023-08-17 08:45:59 -0700

    Added check for hostname to set host-specific files

--- service/startupLoop.sh
+++ service/startupLoop.sh
@@ -12,6 +12,21 @@ else
   echo aborting git pull, host unreachable
 fi
 
+
+# Host-specific settings
+if [[ $HOSTNAME == "pd1" ]]; then
+  echo "set host-specific settings for pd1 here"
+elif [[ $HOSTNAME == "pd2" ]]; then
+  echo "set host-specific settings for pd2 here"
+elif [[ $HOSTNAME == "pd3" ]]; then
+  echo "set host-specific settings for pd3 here"
+elif [[ $HOSTNAME == "pd4" ]]; then
+  echo "set host-specific settings for pd4 here"
+else
+  echo "WARNING: host-name not recognized"
+fi
+
+
 while true; do
   # Check for zombie LXStudio.jar processes and kill them here
   "${PD_ROOT_DIRECTORY}/service/cleanLXStudioProcesses.sh"
