d3ca8f0abe1c4cdfb49d9688fe139fedc767a3f7
jcm-art <87035357+jcm-art@users.noreply.github.com>
2023-09-17 03:06:18 -0700

    Adding current monitoring and clipping for current limited EE setups + serial output (slows performance, sends every 100 LED changes

--- service/arduino_service/LED_FastLED_Pattern/LED_FastLED_Pattern.ino
+++ service/arduino_service/LED_FastLED_Pattern/LED_FastLED_Pattern.ino
@@ -24,9 +24,11 @@ CRGB leds[NUM_FIXTURES][NUM_LEDS_PER_FIXTURE];
 
 // Initialize LED pin list
 constexpr int addressable_pins[] = {ADDRESSABLE_PIN_1, ADDRESSABLE_PIN_2, ADDRESSABLE_PIN_3, ADDRESSABLE_PIN_4, ADDRESSABLE_PIN_5, ADDRESSABLE_PIN_6};
-int output_current = 0;
+float output_current = 0.0;
+int serial_counter = 0;
 
 void setup() {
+  Serial.begin(9600); // open the serial port at 9600 bps:
   // Set pin initial conditions once when you press reset or power the board
   initialize_addressable_LED_pins();
 }
@@ -46,20 +48,32 @@ void initialize_addressable_LED_pins() {
 void set_led(int strip_id, int led_id, int r, int g, int b){
   // Get prior value and update current
   int prior_val = leds[strip_id][led_id].r + leds[strip_id][led_id].g + leds[strip_id][led_id].b;
-  output_current += ((r + g + b) - prior_val)/(3*255) * CURRENT_PER_LED;
+  output_current += (float((r + g + b) - prior_val))/(3*255) * CURRENT_PER_LED;
+
+  if (serial_counter % 100 == 0){
+    Serial.println("Current is " + String(output_current) + " vs. max of " + String(MAX_Current));
+    serial_counter = 0;
+  }
+  serial_counter = serial_counter + 1;
 
   // Set new LED value
   leds[strip_id][led_id] = CRGB(r, g, b);
 
   // Check if current is over limit
   if (output_current > MAX_Current){
+    float adjusted_current = 0.0;
     float current_ratio = MAX_Current / output_current;
     for (int i = 0; i< NUM_FIXTURES; i = i + 1) {
       for (int j = 0; j < NUM_LEDS_PER_FIXTURE; j = j + 1) {
         CRGB prior_color = leds[i][j];
-        leds[i][j] = CRGB(int(prior_color.r*current_ratio), int(prior_color.g*current_ratio), int(prior_color.b*current_ratio));
+        int adj_r = int(prior_color.r*current_ratio);
+        int adj_g = int(prior_color.g*current_ratio);
+        int adj_b = int(prior_color.b*current_ratio);
+        leds[i][j] = CRGB(adj_r, adj_g, adj_b);
+        adjusted_current+=(adj_r+adj_b+adj_g)/(3*255)*CURRENT_PER_LED;
       }
     }
+    output_current = adjusted_current;
   }
 }
 
@@ -67,7 +81,7 @@ void set_led(int strip_id, int led_id, int r, int g, int b){
 void addressable_off(){
   for (byte i = 0; i < NUM_FIXTURES ; i = i + 1) {
     for (byte j = 0; j < NUM_LEDS_PER_FIXTURE; j = j + 1) {
-      leds[i][j] = CRGB(0, 0, 0);
+      set_led(i, j, 0, 0, 0);
     }
   }
   FastLED.show();
@@ -150,10 +164,10 @@ void pulse(int num_iterations, int delay_time, int low_brightness, int high_brig
           set_led(i, j, brightness, 0, 0);
         }
 
-        FastLED.show();
-        brightness = brightness - 1;
-        delay(delay_time);
       }
+      FastLED.show();
+      brightness = brightness - 1;
+      delay(delay_time);
 
     }
   }
@@ -168,7 +182,7 @@ void loop() {
   spiral_traveling_led(2, 100);
   addressable_off();
   delay(500);
-  pulse(2, 100, 10, 50);
+  pulse(2,20, 10, 80);
   addressable_off();
   delay(500);
 
